3f6e4cae72013d7ec3d21c22e3c4719d54f228ea
ram-9047 <ramg786@live.com>
2022-05-29 21:28:43 +0530

    multiple data stored collectively in local storage

--- index.js
+++ index.js
@@ -8,9 +8,31 @@ const userList = document.querySelector("#users");
 // Listen for form submit
 myForm.addEventListener("submit", onSubmit);
 
+// function onSubmit(e) {
+//   e.preventDefault();
+
+//   if (nameInput.value === "" || emailInput.value === "") {
+//     // alert('Please enter all fields');
+//     msg.classList.add("error");
+//     msg.innerHTML = "Please enter all fields";
+
+//     // Remove error after 3 seconds
+//     setTimeout(() => msg.remove(), 3000);
+//   } else {
+//     //Store the user-data in local storage
+//     localStorage.setItem("name", nameInput.value);
+//     localStorage.setItem("email", emailInput.value);
+//     console.log(localStorage);
+//     nameInput.value = "";
+//     emailInput.value = "";
+//   }
+// }
+
+//------------------------------TASK 11 ---------------------
+
+let userInfoList = [];
 function onSubmit(e) {
   e.preventDefault();
-
   if (nameInput.value === "" || emailInput.value === "") {
     // alert('Please enter all fields');
     msg.classList.add("error");
@@ -19,10 +41,21 @@ function onSubmit(e) {
     // Remove error after 3 seconds
     setTimeout(() => msg.remove(), 3000);
   } else {
-    //Store the user-data in local storage
-    localStorage.setItem("name", nameInput.value);
-    localStorage.setItem("email", emailInput.value);
-    console.log(localStorage);
+    //create a new object
+    let myObj = {
+      name: nameInput.value,
+      email: emailInput.value,
+    };
+
+    // push the object into global array
+    userInfoList.push(myObj);
+
+    // store the array in local storage
+    localStorage.setItem("user", JSON.stringify(userInfoList));
+
+    // print the userdata from local storage
+    console.log(JSON.parse(localStorage.user));
+
     nameInput.value = "";
     emailInput.value = "";
   }
