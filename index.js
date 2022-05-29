4e14d3e6ff459988baa3b3ca0f1d544cbdae5772
ram-9047 <ramg786@live.com>
2022-05-30 01:14:30 +0530

    TASK - 12

--- index.js
+++ index.js
@@ -30,6 +30,42 @@ myForm.addEventListener("submit", onSubmit);
 
 //------------------------------TASK 11 ---------------------
 
+// let userInfoList = [];
+// function onSubmit(e) {
+//   e.preventDefault();
+//   if (nameInput.value === "" || emailInput.value === "") {
+//     // alert('Please enter all fields');
+//     msg.classList.add("error");
+//     msg.innerHTML = "Please enter all fields";
+
+//     // Remove error after 3 seconds
+//     setTimeout(() => msg.remove(), 3000);
+//   } else {
+//     //create a new object
+//     let myObj = {
+//       name: nameInput.value,
+//       email: emailInput.value,
+//     };
+
+//     // push the object into global array
+//     userInfoList.push(myObj);
+
+//     // store the array in local storage
+//     localStorage.setItem("user", JSON.stringify(userInfoList));
+
+//     // print the userdata from local storage
+//     console.log(JSON.parse(localStorage.user));
+
+//     nameInput.value = "";
+//     emailInput.value = "";
+//   }
+// }
+
+//-------------------------------TASK - 12 --------------------------------->
+
+window.addEventListener("DOMContentLoaded", (e) => {
+  singleItem();
+});
 let userInfoList = [];
 function onSubmit(e) {
   e.preventDefault();
@@ -53,10 +89,43 @@ function onSubmit(e) {
     // store the array in local storage
     localStorage.setItem("user", JSON.stringify(userInfoList));
 
-    // print the userdata from local storage
-    console.log(JSON.parse(localStorage.user));
-
-    nameInput.value = "";
-    emailInput.value = "";
+    singleItem();
   }
+}
+function singleItem() {
+  let allUser = JSON.parse(localStorage.user);
+  console.log(allUser);
+  //create a single list
+  for (let i = 0; i < allUser.length; i++) {
+    var li = document.createElement("li");
+    let name = allUser[i].name;
+    let email = allUser[i].email;
+    //   console.log(name, email);
+    let editBtn = document.createElement("button");
+    editBtn.appendChild(document.createTextNode("Edit"));
+
+    editBtn.style.marginLeft = "1.5rem";
+    editBtn.style.padding = "0.2rem 1.5rem";
+    editBtn.style.color = "white";
+    editBtn.style.background = "green";
+    editBtn.style.border = "0.2rem solid green";
+    editBtn.style.borderRadius = "0.5rem";
+    let delBtn = document.createElement("button");
+    delBtn.appendChild(document.createTextNode("X"));
+    delBtn.style.marginLeft = "1rem";
+    delBtn.style.padding = "0.2rem 0.5rem";
+    delBtn.style.color = "white";
+    delBtn.style.background = "red";
+    delBtn.style.border = "0.2rem solid red";
+    delBtn.style.borderRadius = "0.4rem";
+
+    li.innerHTML = `${name} ${email}`;
+    li.appendChild(editBtn);
+    li.appendChild(delBtn);
+  }
+  let list = document.querySelector(".items");
+  list.appendChild(li);
+
+  nameInput.value = "";
+  emailInput.value = "";
 }
