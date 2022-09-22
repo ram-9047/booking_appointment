2129ee12c29290f91f7c1dd7f54acf182ddc28b7
ram-9047 <ramg786@live.com>
2022-09-22 21:15:23 +0530

    remove unnecessary comments

--- index.js
+++ index.js
@@ -11,60 +11,6 @@ let update_btn = document.querySelector(".update-btn");
 // myForm.addEventListener("submit", onSubmit);
 submit_btn.addEventListener("click", onSubmit);
 
-// function onSubmit(e) {
-//   e.preventDefault();
-
-//   if (nameInput.value === "" || emailInput.value === "") {
-//     // alert('Please enter all fields');
-//     msg.classList.add("error");
-//     msg.innerHTML = "Please enter all fields";
-
-//     // Remove error after 3 seconds
-//     setTimeout(() => msg.remove(), 3000);
-//   } else {
-//     //Store the user-data in local storage
-//     localStorage.setItem("name", nameInput.value);
-//     localStorage.setItem("email", emailInput.value);
-//     console.log(localStorage);
-//     nameInput.value = "";
-//     emailInput.value = "";
-//   }
-// }
-
-//------------------------------TASK 11 ---------------------
-
-// let userInfoList = [];
-// function onSubmit(e) {
-//   e.preventDefault();
-//   if (nameInput.value === "" || emailInput.value === "") {
-//     // alert('Please enter all fields');
-//     msg.classList.add("error");
-//     msg.innerHTML = "Please enter all fields";
-
-//     // Remove error after 3 seconds
-//     setTimeout(() => msg.remove(), 3000);
-//   } else {
-//     //create a new object
-//     let myObj = {
-//       name: nameInput.value,
-//       email: emailInput.value,
-//     };
-
-//     // push the object into global array
-//     userInfoList.push(myObj);
-
-//     // store the array in local storage
-//     localStorage.setItem("user", JSON.stringify(userInfoList));
-
-//     // print the userdata from local storage
-//     console.log(JSON.parse(localStorage.user));
-
-//     nameInput.value = "";
-//     emailInput.value = "";
-//   }
-// }
-
-//-------------------------------TASK - 12 --------------------------------->
 
 let userInfoObj = [];
 
@@ -95,7 +41,9 @@ function onSubmit(e) {
     userInfoObj.push(myObj);
 
     // store the array in local storage
+    console.log(userInfoObj);
     localStorage.setItem("user", JSON.stringify(userInfoObj));
+    console.log(localStorage.getItem("user"), "after local");
 
     // let items = itemFromLocaStorage();
     displayItem();
@@ -108,7 +56,7 @@ function onSubmit(e) {
 function displayItem() {
   console.log("display");
   let userList = userInfoObj;
-  console.log(userList);
+  // console.log(userList);
   let ul = document.getElementById("users");
   ul.innerHTML = "";
   userList
